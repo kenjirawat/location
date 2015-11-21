a566326a02a790a7465da1304c47b48e504be0fb
kenjirawat <jirawat_ken@hotmail.com>
2015-11-21 10:01:45 +0700

    get location

---
+++ public/todo.js
@@ -0,0 +1,24 @@
+angular.module('todoApp', [])
+  .controller('TodoListController', function ($scope, $http) {
+    var todoList = this
+
+    // Simple GET request example:
+
+    $http.get('/profile').then(function (response) {
+      $scope.name = response.data.displayName
+      $scope.photo = response.data.photos[0].value
+
+    }, function (response) {
+      console.log(response)
+    })
+
+    todoList.post = function () {
+      value = {message: $scope.text}
+      $http.post('/post', value).then(function (response) {
+        console.log(response)
+      }, function (response) {
+        console.log(response)
+      })
+    }
+
+  })
